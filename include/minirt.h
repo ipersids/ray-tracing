61c7e62d51dcfda562c6c701ec02d8d2eb27d379
Julia Llama <71016785+ipersids@users.noreply.github.com>
2025-04-27 11:24:27 +0300

    Revert "Validate arguments and file extension"
    This reverts commit 1b6f7521f9a200a51af8869dc0eb7cfd182cb14b.

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 18:15:50 by ipersids          #+#    #+#             */
-/*   Updated: 2025/04/20 01:18:32 by ipersids         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:35:29 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,6 @@
 # include <unistd.h>			// close, write, read
 # include <stdlib.h>			// malloc, free, exit
 
-# include "minirt_data.h"
 
-/* --------------------- File and arguments validation  -------------------- */
-
-int		rt_validate_args(int argc, char **argv);
-
-/* ---------------------- Error and memory management ---------------------- */
-
-void	rt_perror(int exit_code);
 
 #endif // MINIRT_H
--- src/main.c
+++ src/main.c
@@ -6,18 +6,13 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:55:51 by ipersids          #+#    #+#             */
-/*   Updated: 2025/04/20 01:46:24 by ipersids         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:30:38 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	int	exit_code;
-
-	exit_code = rt_validate_args(argc, argv);
-	if (0 == exit_code)
-		ft_printf("%s\n", argv[1]);
-	return (exit_code);
+	return (0);
 }
