22712532059a7cc5567ef605cf396b9627f298d6
Roni Eerikainen <reerikai@c1r5p19.hive.fi>
2025-06-03 12:17:17 +0300

    Save these

--- src/renderer/patterns.c
+++ src/renderer/patterns.c
@@ -6,7 +6,7 @@
 /*   By: reerikai <reerikai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 18:38:57 by ipersids          #+#    #+#             */
-/*   Updated: 2025/06/02 18:29:38 by reerikai         ###   ########.fr       */
+/*   Updated: 2025/06/03 11:40:14 by reerikai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 
 /* ------------------- Private Function Implementation --------------------- */
 
-t_pat	stripe_pattern(t_color a, t_color b, t_pattype type)
-{
-	t_pat	pattern;
+// t_pat	stripe_pattern(t_color a, t_color b, t_pattype type)
+// {
+// 	t_pat	pattern;
 
-	pattern.type = type;
-	pattern.color_a = a;
-	pattern.color_b = b;
-	pattern.has_pattern = true;
-	pattern.transform = matrix_identity();
-	pattern.inv_transform = inverse(pattern.transform);
-	return (pattern);
-}
+// 	pattern.type = type;
+// 	pattern.color_a = a;
+// 	pattern.color_b = b;
+// 	pattern.has_pattern = true;
+// 	pattern.transform = matrix_identity();
+// 	pattern.inv_transform = inverse(pattern.transform);
+// 	return (pattern);
+// }
 
 t_color	stripe_pattern_at(t_pat *pattern, t_point point)
 {
