e24f11550a076a82b50aaa4bd5e47f38e41494b9
Julia Llama <julia.persidskaia@gmail.com>
2025-06-21 01:14:47 +0300

    Implement multiple lights support

--- src/renderer/color_at.c
+++ src/renderer/color_at.c
@@ -5,34 +5,36 @@
 static t_phong_vars	precompute_data(t_intersection *t, t_ray *ray, t_info *rt);
 static t_color		lighting(t_phong_vars vars, t_light *light, bool in_shadow);
 static void			set_surface_color(t_phong_vars *vars);
+static t_color		compute_fresnel_effect(t_color_at_vars *at, t_phong_vars v);
 
 /* --------------------------- Public Functions ---------------------------- */
 
 t_color	rt_color_at(t_info *rt, t_ray *ray, int ray_bounces)
 {
-	t_intersection	*t;
 	t_phong_vars	v;
 	t_color_at_vars	at;
+	size_t			i;
 
 	rt_intersect_world(rt, ray);
-	t = find_closest_intersection(rt->ts, rt->n_ts);
-	if (NULL == t)
+	at.t = find_closest_intersection(rt->ts, rt->n_ts);
+	if (NULL == at.t)
 		return ((t_color){0.0f, 0.0f, 0.0f});
-	v = precompute_data(t, ray, rt);
+	v = precompute_data(at.t, ray, rt);
 	if (!rt->lights)
 		return (multiply_colors(v.obj->color, rt->amb_intensity));
-	at.shadowed = in_shadow(rt, v.point);
-	at.surface = lighting(v, rt->lights, at.shadowed);
+	i = 0;
+	at.surface = (t_color){0.0f, 0.0f, 0.f};
+	while (i < rt->n_lights)
+	{
+		at.shadowed = in_shadow(rt, v.point);
+		at.tmp_color_surface = lighting(v, &rt->lights[i], at.shadowed);
+		at.surface = addition(at.tmp_color_surface, at.surface);
+		++i;
+	}
 	at.reflected = reflected_color(rt, v, ray_bounces);
 	at.refracted = refracted_color(rt, v, ray_bounces);
 	if (v.obj->material->reflective > 0 && v.obj->material->transparency > 0)
-	{
-		at.reflectance = schlick(v);
-		at.tmp_color_one = multiplication(at.reflected, at.reflectance);
-		at.tmp_color_two = multiplication(at.refracted, 1 - at.reflectance);
-		at.tmp_color_surface = (addition(at.tmp_color_one, at.tmp_color_two));
-		return (addition(at.surface, at.tmp_color_surface));
-	}
+		return (compute_fresnel_effect(&at, v));
 	return (addition(addition(at.surface, at.reflected), at.refracted));
 }
 
@@ -40,26 +42,26 @@ t_color	rt_color_at(t_info *rt, t_ray *ray, int ray_bounces)
 
 static t_phong_vars	precompute_data(t_intersection *t, t_ray *ray, t_info *rt)
 {
-	t_phong_vars	vars;
+	t_phong_vars	v;
 
-	vars.t = t->t;
-	vars.obj = &rt->objs[t->i_object];
-	vars.point = ray_hit(*ray, t->t);
-	vars.eyev = negation(ray->dir);
-	vars.normalv = rt_normal_at(rt, vars.obj, vars.point, t->obj_type);
-	prepare_refraction_calculations(rt, t, &vars.n1, &vars.n2);
-	if (dot_product(vars.normalv, vars.eyev) < 0.0f)
-		vars.normalv = negation(vars.normalv);
-	vars.reflectv = reflect(ray->dir, vars.normalv);
-	vars.under_point = subtraction(
-			vars.point,
-			(multiplication(vars.normalv, SHADOW_BIAS))
+	v.t = t->t;
+	v.obj = &rt->objs[t->i_object];
+	v.point = ray_hit(*ray, t->t);
+	v.eyev = negation(ray->dir);
+	v.normalv = rt_normal_at(rt, v.obj, v.point, t->obj_type);
+	prepare_refraction_calculations(rt, t, &v.n1, &v.n2);
+	if (dot_product(v.normalv, v.eyev) < 0.0f)
+		v.normalv = negation(v.normalv);
+	v.reflectv = reflect(ray->dir, v.normalv);
+	v.under_point = subtraction(
+			v.point,
+			(multiplication(v.normalv, SHADOW_BIAS))
 			);
-	vars.point = addition(vars.point, vars.normalv);
-	if (vars.obj->has_texture == true)
-		vars.texture = rt->win.texture[vars.obj->tex_type];
-	set_surface_color(&vars);
-	return (vars);
+	v.point = addition(v.point, v.normalv);
+	if (v.obj->has_texture == true)
+		v.texture = rt->win.texture[v.obj->tex_type];
+	set_surface_color(&v);
+	return (v);
 }
 
 static t_color	lighting(t_phong_vars vars, t_light *light, bool in_shadow)
@@ -68,17 +70,16 @@ static t_color	lighting(t_phong_vars vars, t_light *light, bool in_shadow)
 
 	if (!light)
 		return (vars.obj->amb_component);
-	pc.eff_col = multiply_colors(vars.surface_color, light[0].intensity);
-	// pc.lightv = normalize(subtraction(light[0].pos, vars.point));
+	pc.eff_col = multiply_colors(vars.surface_color, light->intensity);
 	pc.amb = multiply_colors(pc.eff_col, vars.obj->amb_component);
 	if (in_shadow)
 		return (pc.amb);
-	pc.lightv = normalize(subtraction(light[0].pos, vars.point));
+	pc.lightv = normalize(subtraction(light->pos, vars.point));
 	pc.l_dot_norm = dot_product(pc.lightv, vars.normalv);
 	if (light_behind_surface(pc.l_dot_norm))
 		set_dark(&pc);
 	else
-		set_color(&pc, vars.obj->material, &vars, light[0]);
+		set_color(&pc, vars.obj->material, &vars, *light);
 	return (addition(addition(pc.amb, pc.dif), pc.spec));
 }
 
@@ -99,4 +100,13 @@ static void	set_surface_color(t_phong_vars *vars)
 	}
 	else
 		vars->surface_color = vars->obj->color;
+}
+
+static t_color	compute_fresnel_effect(t_color_at_vars *at, t_phong_vars v)
+{
+	at->reflectance = schlick(v);
+	at->tmp_color_one = multiplication(at->reflected, at->reflectance);
+	at->tmp_color_two = multiplication(at->refracted, 1 - at->reflectance);
+	at->tmp_color_surface = (addition(at->tmp_color_one, at->tmp_color_two));
+	return (addition(at->surface, at->tmp_color_surface));
 }
